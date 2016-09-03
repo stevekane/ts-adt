aa96c74b866116b578c3ad2dd37beebc4ee2e68f
Steve Kane <kanesteven@gmail.com>
2016-09-03 14:01:24 -0500

    tests added for Either

--- test.ts
+++ test.ts
@@ -11,9 +11,24 @@ test('Maybe', (t) => {
   const m3 = Maybe.unit(5)
 
   t.true(m instanceof Maybe.Just && m.val === 10)
-  t.true(n instanceof Maybe.Nothing, 'n is Nothing'),
-  t.true(m2 instanceof Maybe.Just, 'm is Just(5)'),
-  t.true(n2 instanceof Maybe.Nothing, 'n is Nothing'),
-  t.true(m3 instanceof Maybe.Just && m3.val === 5, 'm3 is Just(5)')
+  t.true(n instanceof Maybe.Nothing)
+  t.true(m2 instanceof Maybe.Just)
+  t.true(n2 instanceof Maybe.Nothing)
+  t.true(m3 instanceof Maybe.Just && m3.val === 5)
+  t.end()
+})
+
+test('Either', (t) => {
+  const m = Either.fmap((v: number) => v + 5, new Either.Success(5))
+  const n = Either.fmap((v: number) => v + 5, new Either.Failure('Error'))
+  const m2 = Either.flatMap((v: number) => new Either.Success(v + 5), new Either.Success(5))
+  const n2 = Either.flatMap((v: number) => new Either.Success(v + 5), new Either.Failure('Err'))
+  const m3 = Either.unit(5)
+
+  t.true(m instanceof Either.Success && m.val === 10)
+  t.true(n instanceof Either.Failure && n.message === 'Error')
+  t.true(m2 instanceof Either.Success)
+  t.true(n2 instanceof Either.Failure && n2.message === 'Err')
+  t.true(m3 instanceof Either.Success && m3.val === 5)
   t.end()
 })
