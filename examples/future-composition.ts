fe60dfe926dea197913f6ed7aca40df335aebb9a
Steve Kane <kanesteven@gmail.com>
2016-09-07 13:51:08 -0500

    added more steps to future example

--- examples/future-composition.ts
+++ examples/future-composition.ts
@@ -18,6 +18,12 @@ type PatientData = {
   provider?: string
 }
 
+type Rating = { category: string, value: number }
+
+function randIntRange (min: number, max: number): number {
+  return min + Math.round(Math.random() * (max - min))
+}
+
 function fetchPatientData (id: number): Future<PatientData> {
   console.log('Fetching patient data from server...')
   return new Future(r => setTimeout(r, 1000, {
@@ -40,9 +46,17 @@ function WorkSheet (patientData: PatientData): Future<PatientData> {
   return new Future(r => setTimeout(r, duration, {
     firstName: 'Steve',
     lastName: 'Kane',
-    age: Math.floor(Math.random() * 32),
+    age: randIntRange(1, 32),
     provider: 'Mercy'
   }))
+}
+
+function DrugUseRatings (): Future<Rating[]> {
+  console.log('Asking patient to rate their frequency of drug use...')
+  return new Future(r => setTimeout(r, 5000, [
+    { category: 'meth', value: randIntRange(0, 10) }, 
+    { category: 'alchohol', value: randIntRange(0, 10) }
+  ]))
 }
 
 function MinorOutro (firstName: string): Future<number> {
@@ -58,8 +72,9 @@ function AdultOutro (firstName: string): Future<number> {
 const patientId = 5
 const program = fetchPatientData(patientId)
                 .chain(initialPatientData => Intro()
-                .chain(_ => WorkSheet(initialPatientData))
-                .chain(updatedPatientData => (updatedPatientData.age < 18 ? MinorOutro(updatedPatientData.firstName) : AdultOutro(updatedPatientData.firstName))
-                .chain(_2 => Future.unit({ patientData: updatedPatientData, risks: [] }))))
+                .chain(_ => WorkSheet(initialPatientData)
+                .chain(updatedPatientData => DrugUseRatings()
+                .chain(drugUseRatings => (updatedPatientData.age < 18 ? MinorOutro(updatedPatientData.firstName) : AdultOutro(updatedPatientData.firstName))
+                .chain(_2 => Future.unit({ patientData: updatedPatientData, risks: [] }))))))
 
 program.fork(console.log.bind(console))
