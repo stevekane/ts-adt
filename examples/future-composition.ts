3ab45811c0bbdde4478c1671f04b2b947ea6c74d
Steve Kane <kanesteven@gmail.com>
2016-09-08 16:23:43 -0500

    minor tweaks to future composition

--- examples/future-composition.ts
+++ examples/future-composition.ts
@@ -3,17 +3,17 @@
 import { Future } from '../src/Future'
 
 /*
-  In this world, all entities are Futures and thusly are interface-wise
-  indistinguishable from one-another.  The base parts are
-  1.  Scene 
-  2.  LogicNode
-  3.  OrderedFlow
-  4.  Sequential Compositions of Any of these
+  In this world, all entities are Futures and are interface-wise
+  indistinguishable from one-another.
+
+  Since Future<T> implements Monad<T> we can compose Futures
+  out of other Futures to build up modules, sections, control flows
+  programs, editions, etc
 */
 
 type PatientData = {
   firstName: string
-  lastname: string
+  lastName: string
   age?: number
   provider?: string
 }
@@ -28,6 +28,8 @@ interface Rating {
   category: DRUG_CATEGORY
   value: number
 }
+
+const log = console.log.bind(console)
 
 function randIntRange (min: number, max: number): number {
   return min + Math.round(Math.random() * (max - min))
@@ -45,60 +47,63 @@ function onlyHigh (a: { value: number }): boolean {
   return a.value > 5
 }
 
-
 function fetchPatientData (id: number): Future<PatientData> {
-  console.log('Fetching patient data from server...')
-  return new Future(r => setTimeout(r, 1000, {
+  const msg = 'Fetching patient data from server...'
+
+  return new Future(r => (log(msg), setTimeout(r, 1000, {
     firstName: 'Steve',
     lastName: 'Kane'
-  }))
+  })))
+}
+
+function Intro (): Future<number> {
+  const duration = 2000
+  const msg = 'Intro...'
+
+  return new Future(r => (log(msg), setTimeout(r, duration, 1)))
+}
+
+function WorkSheet (patientData: PatientData): Future<PatientData> {
+  const duration = 3000
+  const msg = 'Asking patient to update their information...'
+
+  return new Future(r => (log(msg), setTimeout(r, duration, {
+    firstName: 'Steve',
+    lastName: 'Kane',
+    age: randIntRange(1, 32),
+    provider: 'Mercy'
+  })))
+}
+
+function DrugUseRatings (): Future<Rating[]> {
+  const msg = 'Asking patient to rate their frequency of drug use...'
+
+  return new Future(r => (log(msg), setTimeout(r, 5000, [
+    { category: 'METH', value: randIntRange(0, 10) }, 
+    { category: 'ALCOHOL', value: randIntRange(0, 10) },
+    { category: 'MARIJUANA', value: randIntRange(0, 10) },
+    { category: 'COCAINE', value: randIntRange(0, 10) },
+    { category: 'HEROINE', value: randIntRange(0, 10) }
+  ])))
 }
 
 function DrugRisk (rating: Rating): Future<number> {
   const duration = 3000
   const msg = `Informing patient of risks from frequent use of ${ rating.category }...`
 
-  return new Future(r => (console.log(msg), setTimeout(r, duration, 1)))
-}
-
-function Intro (): Future<number> {
-  const duration = 2000
-
-  console.log(`Intro...`)
-  return new Future(r => { setTimeout(r, duration, 1) })
-}
-
-function WorkSheet (patientData: PatientData): Future<PatientData> {
-  const duration = 3000
-
-  console.log(`Asking patient to update their information...`)
-  return new Future(r => setTimeout(r, duration, {
-    firstName: 'Steve',
-    lastName: 'Kane',
-    age: randIntRange(1, 32),
-    provider: 'Mercy'
-  }))
-}
-
-function DrugUseRatings (): Future<Rating[]> {
-  console.log('Asking patient to rate their frequency of drug use...')
-  return new Future(r => setTimeout(r, 5000, [
-    { category: 'METH', value: randIntRange(0, 10) }, 
-    { category: 'ALCOHOL', value: randIntRange(0, 10) },
-    { category: 'MARIJUANA', value: randIntRange(0, 10) },
-    { category: 'COCAINE', value: randIntRange(0, 10) },
-    { category: 'HEROINE', value: randIntRange(0, 10) }
-  ]))
+  return new Future(r => (log(msg), setTimeout(r, duration, 1)))
 }
 
 function MinorOutro (firstName: string): Future<number> {
-  console.log('Showing outro for a minor')
-  return new Future(r => { setTimeout(r, 2000, 2) })
+  const msg = 'Showing outro for a minor...'
+
+  return new Future(r => (log(msg), setTimeout(r, 2000, 2)))
 }
 
 function AdultOutro (firstName: string): Future<number> {
-  console.log('Showing outro for an adult')
-  return new Future(r => setTimeout(r, 3000, 2))
+  const msg = 'Showing outro for an adult...'
+
+  return new Future(r => (log(msg), setTimeout(r, 3000, 2)))
 }
 
 function sequence<A> (fs: Future<A>[]): Future<A[]> {
@@ -112,27 +117,14 @@ function map2<A, B, R> (fn: (a: A, b: B) => R, fA: Future<A>, fB: Future<B>): Fu
   return fA.chain(a => fB.chain(b => Future.unit(fn(a, b))))
 }
 
-/*
-sequence : List (Task x a) -> Task x (List a)
-sequence tasks =
-  case tasks of 
-    []                     -> succeed []
-    task :: remainingTasks -> map2 (::) task (sequence remainingTasks)
-
-  map2 : (a -> b -> result) -> Task x a -> Task x b -> Task x result
-  map2 func taskA taskB =
-    taskA
-      `andThen` \a -> taskB
-      `andThen` \b -> succeed (func a b)
-*/
-
 const patientId = 5
-const program = fetchPatientData(patientId)
-                .chain(initialPatientData => Intro()
-                .chain(_ => WorkSheet(initialPatientData)
-                .chain(updatedPatientData => DrugUseRatings()
-                .chain(drugUseRatings => sequence(drugUseRatings.sort(byValue).filter(onlyHigh).map(DrugRisk))
-                .chain(_2 => (updatedPatientData.age < 18 ? MinorOutro(updatedPatientData.firstName) : AdultOutro(updatedPatientData.firstName))
-                .chain(_3 => Future.unit({ patientData: updatedPatientData, risks: drugUseRatings })))))))
+const program
+  = fetchPatientData(patientId)
+    .chain(initialPatientData => Intro()
+      .chain(_                  => WorkSheet(initialPatientData)
+        .chain(updatedPatientData => DrugUseRatings()
+          .chain(drugUseRatings     => sequence(drugUseRatings.sort(byValue).filter(onlyHigh).map(DrugRisk))
+            .chain(_2                 => (updatedPatientData.age < 18 ? MinorOutro : AdultOutro)(updatedPatientData.firstName)
+              .chain(_3                 => Future.unit({ patientData: updatedPatientData, highRisks: drugUseRatings.filter(onlyHigh) })))))))
 
-program.fork(console.log.bind(console))
+program.fork(log.bind(console))
