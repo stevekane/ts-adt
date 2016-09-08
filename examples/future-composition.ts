5e3333670b15e6864a1739e2fd8818edea786fe6
Steve Kane <kanesteven@gmail.com>
2016-09-08 11:18:45 -0500

    future composition example now includes sequencing dynamic lists of futures

--- examples/future-composition.ts
+++ examples/future-composition.ts
@@ -17,12 +17,34 @@ type PatientData = {
   age?: number
   provider?: string
 }
+type DRUG_CATEGORY
+  = 'METH'
+  | 'ALCOHOL'
+  | 'MARIJUANA'
+  | 'COCAINE'
+  | 'HEROINE'
 
-type Rating = { category: string, value: number }
+interface Rating { 
+  category: DRUG_CATEGORY
+  value: number
+}
 
 function randIntRange (min: number, max: number): number {
   return min + Math.round(Math.random() * (max - min))
 }
+
+function concat<A> (a: A, list: A[]): A[] {
+  return list.concat(a)
+}
+
+function byValue (a: { value: number }, b: { value: number }): number {
+  return a.value - b.value
+}
+
+function onlyHigh (a: { value: number }): boolean {
+  return a.value > 5
+}
+
 
 function fetchPatientData (id: number): Future<PatientData> {
   console.log('Fetching patient data from server...')
@@ -30,6 +52,13 @@ function fetchPatientData (id: number): Future<PatientData> {
     firstName: 'Steve',
     lastName: 'Kane'
   }))
+}
+
+function DrugRisk (rating: Rating): Future<number> {
+  const duration = 3000
+  const msg = `Informing patient of risks from frequent use of ${ rating.category }...`
+
+  return new Future(r => (console.log(msg), setTimeout(r, duration, 1)))
 }
 
 function Intro (): Future<number> {
@@ -54,8 +83,11 @@ function WorkSheet (patientData: PatientData): Future<PatientData> {
 function DrugUseRatings (): Future<Rating[]> {
   console.log('Asking patient to rate their frequency of drug use...')
   return new Future(r => setTimeout(r, 5000, [
-    { category: 'meth', value: randIntRange(0, 10) }, 
-    { category: 'alchohol', value: randIntRange(0, 10) }
+    { category: 'METH', value: randIntRange(0, 10) }, 
+    { category: 'ALCOHOL', value: randIntRange(0, 10) },
+    { category: 'MARIJUANA', value: randIntRange(0, 10) },
+    { category: 'COCAINE', value: randIntRange(0, 10) },
+    { category: 'HEROINE', value: randIntRange(0, 10) }
   ]))
 }
 
@@ -66,15 +98,41 @@ function MinorOutro (firstName: string): Future<number> {
 
 function AdultOutro (firstName: string): Future<number> {
   console.log('Showing outro for an adult')
-  return new Future(r => { setTimeout(r, 3000, 2) })
+  return new Future(r => setTimeout(r, 3000, 2))
 }
+
+function sequence<A> (fs: Future<A>[]): Future<A[]> {
+  const [ t, ...remaining ] = fs
+
+  if ( fs.length === 0 ) return Future.unit([]) 
+  else                   return map2(concat, t, sequence(remaining))
+}
+
+function map2<A, B, R> (fn: (a: A, b: B) => R, fA: Future<A>, fB: Future<B>): Future<R> {
+  return fA.chain(a => fB.chain(b => Future.unit(fn(a, b))))
+}
+
+/*
+sequence : List (Task x a) -> Task x (List a)
+sequence tasks =
+  case tasks of 
+    []                     -> succeed []
+    task :: remainingTasks -> map2 (::) task (sequence remainingTasks)
+
+  map2 : (a -> b -> result) -> Task x a -> Task x b -> Task x result
+  map2 func taskA taskB =
+    taskA
+      `andThen` \a -> taskB
+      `andThen` \b -> succeed (func a b)
+*/
 
 const patientId = 5
 const program = fetchPatientData(patientId)
                 .chain(initialPatientData => Intro()
                 .chain(_ => WorkSheet(initialPatientData)
                 .chain(updatedPatientData => DrugUseRatings()
-                .chain(drugUseRatings => (updatedPatientData.age < 18 ? MinorOutro(updatedPatientData.firstName) : AdultOutro(updatedPatientData.firstName))
-                .chain(_2 => Future.unit({ patientData: updatedPatientData, risks: drugUseRatings.sort((a, b) => a.value - b.value) }))))))
+                .chain(drugUseRatings => sequence(drugUseRatings.sort(byValue).filter(onlyHigh).map(DrugRisk))
+                .chain(_2 => (updatedPatientData.age < 18 ? MinorOutro(updatedPatientData.firstName) : AdultOutro(updatedPatientData.firstName))
+                .chain(_3 => Future.unit({ patientData: updatedPatientData, risks: drugUseRatings })))))))
 
 program.fork(console.log.bind(console))
