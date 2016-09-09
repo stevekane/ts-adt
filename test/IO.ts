f43faf7615e5e1bb8f575cd7e65dc0e1cd8d6adc
Steve Kane <kanesteven@gmail.com>
2016-09-09 11:42:32 -0500

    restored commented out test for map on IO

--- test/IO.ts
+++ test/IO.ts
@@ -5,7 +5,7 @@ test('IO', t => {
   const processInfo = (): IO<NodeJS.Process> => IO.unit(process)
 
   t.same(IO.unit(5).run(), 5)
-  // t.same(IO.unit(5).map(v => v + 5).run()), 10)
+  t.same(IO.unit(5).map(v => v + 5).run(), 10)
   t.same(IO.unit(5).chain(v => IO.unit(v + 5)).run(), 10)
   t.same(processInfo().run().env.PWD, process.env.PWD)
   t.end() 
