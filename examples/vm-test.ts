34c9d5b4bc60ff43aca6116bbd55f5c562b856ee
Steve Kane <kanesteven@gmail.com>
2016-09-22 12:14:29 -0500

    initial version of DynamicDispatch version of VM

--- examples/vm-test.ts
+++ examples/vm-test.ts
@@ -1,56 +1,29 @@
 import * as test from 'tape'
-import { VM, INS, step } from './vm'
-
-function toName (i: INS): string {
-  switch ( i ) {
-    case INS.CONST:  return 'CONST' 
-    case INS.ADD:    return 'ADD' 
-    case INS.EQ:     return 'EQ' 
-    case INS.LOAD:   return 'LOAD' 
-    case INS.STORE:  return 'STORE' 
-    case INS.CALL:   return 'CALL' 
-    case INS.RET:    return 'RET' 
-    default:         return 'UNKNOWN OP'
-  }
-}
-
-function traceVM (ins: number, vm: VM<INS, number>) {
-  const op = `OP: ${ toName(ins) }`
-  const stack = `STACK: ${ vm.stack.slice(0, vm.sp + 1).join(' ') }`
-
-  console.log([ op, stack ].join('\n') + '\n')
-}
-
-function run (vm: VM<INS, number>) {
-  while (vm.ip >= 0 && vm.ip < vm.code.length) {
-    const op = vm.code[vm.ip]
-
-    step(vm) 
-    traceVM(op, vm)
-  }
-}
+import { VM, INS, Instructions } from './vm'
 
 test('VM', t => {
-  const p1 = [
-    // Add fn
-    INS.LOAD, -4,
-    INS.LOAD, -3,
-    INS.ADD,
-    INS.RET,
-    // Main
-    INS.CONST, 1,
-    INS.CONST, 2,
-    INS.CALL, 0, 2
+  const p = [
+    // fn add
+    new Instructions.LOAD(-4),
+    new Instructions.LOAD(-3),
+    new Instructions.ADD,
+    new Instructions.RET,
+    // main
+    new Instructions.CONST('CAT'),
+    new Instructions.CONST('BERT'),
+    new Instructions.CALL(0, 2)
   ]
-  const vm: VM<INS, number> = {
+  const vm = {
     sp: -1,
     fp: 0,
-    ip: 6, // main for p1
+    ip: 4,
     stack: new Array(20).fill(0),
-    code: p1
+    code: p
   }
-  run(vm)
 
-  t.same(vm.stack[vm.sp], 3)
+  while ( vm.ip >= 0 && vm.ip < vm.code.length ) {
+    vm.code[vm.ip].run(vm)
+  }
+  t.same(vm.stack[vm.sp], 'CATBERT')
   t.end()
 })
--- examples/vm.ts
+++ examples/vm.ts
@@ -1,65 +1,81 @@
-export enum INS { CONST, ADD, EQ, LOAD, STORE, CALL, RET } 
-
-export interface VM<I, T> {
+export interface VM {
   sp: number
   fp: number
   ip: number
-  stack: T[]
-  code: I[]
+  stack: any[]
+  code: INS[]
 }
 
-/*
-Eliminate reg
-Eliminate fixed-size stack
-Eliminate sp?
-INS Should be interface-satisfying classes?
-*/
+export interface INS { 
+  run(vm: VM): void
+}
 
-export function step (vm: VM<INS, number>) {
-  switch ( vm.code[vm.ip] ) {
-    case INS.CONST: 
-      vm.stack[++vm.sp] = vm.code[vm.ip + 1]
-      vm.ip += 2 // Could change above to ++ and here to ++
-      break
-    case INS.ADD:
-      var sum = vm.stack[vm.sp--] + vm.stack[vm.sp--]
-      vm.stack[++vm.sp] = sum
+export namespace Instructions {
+  export class CONST implements INS { 
+    constructor(public val: any) {}
+    run(vm: VM) {
+      vm.stack[++vm.sp] = this.val
       vm.ip++
-      break
-    case INS.EQ:
-      var eq = vm.stack[vm.sp--] == vm.stack[vm.sp--] ? 1 : 0
+    }
+  }
+
+  export class ADD implements INS {
+    run(vm: VM) {
+      const right = vm.stack[vm.sp--]
+      const left = vm.stack[vm.sp--]
+
+      vm.stack[++vm.sp] = left + right
+      vm.ip++
+    }
+  }
+
+  export class EQ implements INS {
+    run(vm: VM) {
+      const eq = vm.stack[vm.sp--] == vm.stack[vm.sp--] ? 1 : 0
+
       vm.stack[++vm.sp] = eq
       vm.ip++
-      break
-    case INS.CALL:
-      var fromAddr = vm.code[++vm.ip]
-      var numArgs = vm.code[++vm.ip]
-      vm.stack[++vm.sp] = numArgs
+    }
+  }
+
+  export class CALL implements INS {
+    constructor(public fnAddr: number, public numArgs: number) {}
+    run(vm: VM) {
+      vm.stack[++vm.sp] = this.numArgs
       vm.stack[++vm.sp] = vm.fp
       vm.stack[++vm.sp] = vm.ip + 1
       vm.fp = vm.sp
-      vm.ip = fromAddr
-      break
-    case INS.RET:
-      var val = vm.stack[vm.sp--]
+      vm.ip = this.fnAddr
+    }
+  }
+
+  export class RET implements INS {
+    run(vm: VM) {
+      const val = vm.stack[vm.sp--]
+
       vm.sp = vm.fp
       vm.ip = vm.stack[vm.sp--]
       vm.fp = vm.stack[vm.sp--]
-      var argCount = vm.stack[vm.sp--]
+      const argCount = vm.stack[vm.sp--]
       vm.sp -= argCount
       vm.stack[++vm.sp] = val
-      break
-    case INS.LOAD:
-      var offset = vm.code[++vm.ip]
-      vm.stack[++vm.sp] = vm.stack[vm.fp + offset]
+    }
+  }
+
+  export class LOAD implements INS {
+    constructor(public offset: number) {}
+    run(vm: VM) {
+      vm.stack[++vm.sp] = vm.stack[vm.fp + this.offset]
       vm.ip++
-      break
-    case INS.STORE:
-      var offset = vm.code[vm.ip++]
-      var val = vm.stack[vm.sp--]
-      vm.stack[vm.fp + offset] = val
-      break
-    default:       
-      throw new Error('Unexpected Operation.... Should be un-reachable')
-  }  
+    }
+  }
+
+  export class STORE implements INS {
+    constructor(public offset: number) {}
+    run(vm: VM) {
+      const val = vm.stack[vm.sp--]
+
+      vm.stack[vm.fp + this.offset] = val
+    }
+  }
 }
