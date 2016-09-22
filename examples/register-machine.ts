e63d77701ebf34819842d1d79a570e65ffb47da6
Steve Kane <kanesteven@gmail.com>
2016-09-22 14:07:18 -0500

    decoposed commands into type-safe commands

--- examples/register-machine.ts
+++ examples/register-machine.ts
@@ -1,5 +1,3 @@
-export type Primitive = number | string | boolean
-
 export interface IRegisterVM {
   str: string[] 
   num: number[]
@@ -15,18 +13,54 @@ export class RegVM implements IRegisterVM {
   constructor(public ip: number, public code: INS[]) {}
 }
 
-export class CONST {
-  kind: 'CONST' = 'CONST'
-  constructor(public val: Primitive, public t: number) {}
+
+// CONST
+export class CONST_NUM {
+  kind: 'CONST_NUM' = 'CONST_NUM'
+  constructor(public val: number, public t: number) {}
 }
 
+export class CONST_STR {
+  kind: 'CONST_STR' = 'CONST_STR'
+  constructor(public val: string, public t: number) {}
+}
+
+export class CONST_BOOL {
+  kind: 'CONST_BOOL' = 'CONST_BOOL'
+  constructor(public val: boolean, public t: number) {}
+}
+
+
+// PRINT
 export class PRINT_STR {
   kind: 'PRINT_STR' = 'PRINT_STR'
   constructor(public f: number) {}
 }
 
-export class CONCAT_STR {
-  kind: 'CONCAT_STR' = 'CONCAT_STR'
+export class PRINT_NUM {
+  kind: 'PRINT_NUM' = 'PRINT_NUM'
+  constructor(public f: number) {}
+}
+
+export class PRINT_BOOL {
+  kind: 'PRINT_BOOL' = 'PRINT_BOOL'
+  constructor(public f: number) {}
+}
+
+
+// MAPPEND ( concat, add, && )
+export class MAPPEND_STR {
+  kind: 'MAPPEND_STR' = 'MAPPEND_STR'
+  constructor(public f1: number, public f2: number, public t: number) {}
+}
+
+export class MAPPEND_NUM {
+  kind: 'MAPPEND_NUM' = 'MAPPEND_NUM'
+  constructor(public f1: number, public f2: number, public t: number) {}
+}
+
+export class MAPPEND_BOOL {
+  kind: 'MAPPEND_BOOL' = 'MAPPEND_BOOL'
   constructor(public f1: number, public f2: number, public t: number) {}
 }
 
@@ -34,16 +68,15 @@ export class HALT {
   kind: 'HALT' = 'HALT'
 }
 
-type INS 
-  = CONST
-  | PRINT_STR
-  | CONCAT_STR
-  | HALT
+type CONST = CONST_STR | CONST_NUM | CONST_BOOL
+type PRINT = PRINT_STR | PRINT_NUM | PRINT_BOOL
+type MAPPEND = MAPPEND_STR | MAPPEND_NUM | MAPPEND_BOOL
+type INS = CONST | PRINT | MAPPEND | HALT
 
 const p = [
-  new CONST('hello', 0), 
-  new CONST('world', 1),
-  new CONCAT_STR(0, 1, 2),
+  new CONST_STR('hello', 0), 
+  new CONST_STR('world', 1),
+  new MAPPEND_STR(0, 1, 2),
   new PRINT_STR(0),
   new HALT
 ]
@@ -52,14 +85,16 @@ const vm = new RegVM(0, p)
 function step(vm: IRegisterVM) {
   const ins = vm.code[vm.ip++]
 
-  if ( ins.kind == 'CONST' ) {
-    if      ( typeof ins.val == 'string' )  vm.str[ins.t] = ins.val
-    else if ( typeof ins.val == 'boolean' ) vm.bool[ins.t] = ins.val
-    else                                    vm.num[ins.t] = ins.val
-  }
-  else if ( ins.kind == 'PRINT_STR' )       console.log(vm.str[ins.f])
-  else if ( ins.kind == 'CONCAT_STR' )      vm.str[ins.t] = vm.str[ins.f1] + vm.str[ins.f2]
-  else                                      vm.ip = -1
+  if      ( ins.kind == 'CONST_NUM' )    vm.num[ins.t] = ins.val
+  else if ( ins.kind == 'CONST_STR' )    vm.str[ins.t] = ins.val
+  else if ( ins.kind == 'CONST_BOOL')    vm.bool[ins.t] = ins.val
+  else if ( ins.kind == 'MAPPEND_NUM' )  vm.num[ins.t] = vm.num[ins.f1] + vm.num[ins.f2]
+  else if ( ins.kind == 'MAPPEND_STR' )  vm.str[ins.t] = vm.str[ins.f1] + vm.str[ins.f2]
+  else if ( ins.kind == 'MAPPEND_BOOL' ) vm.bool[ins.t] = vm.bool[ins.f1] && vm.bool[ins.f2]
+  else if ( ins.kind == 'PRINT_NUM' )    console.log(vm.num[ins.f]) 
+  else if ( ins.kind == 'PRINT_STR' )    console.log(vm.str[ins.f])
+  else if ( ins.kind == 'PRINT_BOOL' )   console.log(vm.bool[ins.f])
+  else                                   vm.ip = -1
 }
 
 while ( vm.ip >= 0 && vm.ip < vm.code.length ) step(vm)
